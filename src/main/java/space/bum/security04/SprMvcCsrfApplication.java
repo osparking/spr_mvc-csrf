cfee817f5026358d4555adfbfbbe7d9249ed71d0
osparking <jbpark03@gmail.com>
2024-04-20 15:12:36 +0900

    봄 부트 종속성 + 교습서 종속성

---
+++ src/main/java/space/bum/security04/SprMvcCsrfApplication.java
@@ -0,0 +1,13 @@
+package space.bum.security04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprMvcCsrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprMvcCsrfApplication.class, args);
+	}
+
+}
